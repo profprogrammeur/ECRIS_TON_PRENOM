bf0102cdaff4baab10c98c1c13d9cbf93d8c0025
Naoxys <souleyk3@gmail.com>
2021-12-08 15:35:41 +0100

    add model Game & Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_123142) do
+ActiveRecord::Schema.define(version: 2021_12_08_141902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "child_name"
