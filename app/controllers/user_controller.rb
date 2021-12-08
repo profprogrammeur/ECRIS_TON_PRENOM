72dd45ff50b32fdeca9ef86668236f4ebc10c6b0
Naoxys <souleyk3@gmail.com>
2021-12-08 16:43:19 +0100

    amended Controller User&Game

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,19 +1,35 @@
 class UserController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:user.:id => true])
   end
+  
 
   def new
+    @user = User.new
   end
+  
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Object successfully created"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
   end
+  
 
   def edit
+    @user = User.find(params[:user.:id => true])
   end
+  
 
   def update
   end
