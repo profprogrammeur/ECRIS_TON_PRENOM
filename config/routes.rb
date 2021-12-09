e5ae3d7d361dc39cc4a0ebbb51246c4c48cb38e0
Naoxys <souleyk3@gmail.com>
2021-12-09 21:24:27 +0100

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
-
-  root to: 'users#index'
+  root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users
   resources :games
   resources :carts 
