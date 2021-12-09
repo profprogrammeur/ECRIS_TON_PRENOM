f963c2952aa2f080fa041de80178230154f9a7a1
profprogrammeur <contact@profprogrammeur.com>
2021-12-09 07:13:58 +0100

    comment send email welcome

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :welcome_send
+  # after_create :welcome_send
 
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
