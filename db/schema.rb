f8767200267319f046fc0a9526ce82292169d547
Naoxys <souleyk3@gmail.com>
2021-12-17 14:44:25 +0100

    modification route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_204135) do
+ActiveRecord::Schema.define(version: 2021_12_17_132733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_12_14_204135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_carts_on_item_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
@@ -52,6 +54,13 @@ ActiveRecord::Schema.define(version: 2021_12_14_204135) do
     t.index ["item_id"], name: "index_join_table_cart_items_on_item_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "child_name"
     t.datetime "created_at", null: false
@@ -65,5 +74,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_204135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "items"
   add_foreign_key "carts", "users"
 end
