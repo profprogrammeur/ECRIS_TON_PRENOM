87173de54e09942f790fa8a74f85f1b2ce190383
Naoxys <souleyk3@gmail.com>
2021-12-17 01:32:00 +0100

    modification premium

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         after_create :welcome_send
- has_many :cart 
+         #after_create :welcome_send
+  has_many :cart 
   has_many :games
   
   
