013684e148aa6944ee0ce33257f80e0113311ad9
profprogrammeur <contact@profprogrammeur.com>
2021-12-19 17:38:43 +0100

    comment welcome_send

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :games
   has_many :items, through: :cart
 
-  after_create :welcome_send
+  # after_create :welcome_send
 
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
