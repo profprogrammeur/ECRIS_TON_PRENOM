84cc2cb599ab82ca860dad8f4bb254b7b33fbd81
profprogrammeur <contact@profprogrammeur.com>
2021-12-20 09:47:36 +0100

    config_action mailer deliveries true in prod + uncomment welcome email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ETP_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'ecris-ton-prenom.herokuapp.com' }
+  # config.action_mailer.default_url_options = { :host => 'ecris-ton-prenom.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
